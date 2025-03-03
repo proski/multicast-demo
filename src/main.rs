6dcb34ff71869a5c13c1d27f03cf03d9f6f15eaa
Pavel Roskin <plroskin@gmail.com>
2025-03-02 20:31:41 -0800

    Populate receiver 2, improve logging, use overlapping ranges

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn receiver1(running: Arc<AtomicBool>) -> Result<()> {
     socket.set_multicast_all_v4(false)?;
     socket.set_read_timeout(Some(Duration::from_millis(50)))?;
     socket.bind(&RX_SOCKADDR.into())?;
-    for i in 1..3 {
+    for i in 1..=5 {
         let multicast_addr: Ipv4Addr = format!("239.0.0.{i}").as_str().parse()?;
         info!(?multicast_addr, "receiver 1 joining");
         socket.join_multicast_v4(&multicast_addr, &RX_ADDRESS)?;
@@ -33,12 +33,33 @@ fn receiver1(running: Arc<AtomicBool>) -> Result<()> {
             continue;
         };
         let data = str::from_utf8(&buffer[0..packet_size])?;
-        info!(?data, ?packet_size, ?sockaddr, "receiver 1 received");
+        info!(?data, ?sockaddr, "receiver 1:");
     }
     Ok(())
 }
 
-fn receiver2(_running: Arc<AtomicBool>) -> Result<()> {
+fn receiver2(running: Arc<AtomicBool>) -> Result<()> {
+    let socket = Socket::new(Domain::IPV4, Type::DGRAM, None)?;
+    socket.set_reuse_address(true)?;
+    socket.set_multicast_all_v4(false)?;
+    socket.set_read_timeout(Some(Duration::from_millis(50)))?;
+    socket.bind(&RX_SOCKADDR.into())?;
+    for i in 3..=7 {
+        let multicast_addr: Ipv4Addr = format!("239.0.0.{i}").as_str().parse()?;
+        info!(?multicast_addr, "receiver 2 joining");
+        socket.join_multicast_v4(&multicast_addr, &RX_ADDRESS)?;
+    }
+
+    let socket: UdpSocket = socket.into();
+    let mut buffer = vec![0_u8; 65536];
+
+    while running.load(Ordering::Relaxed) {
+        let Ok((packet_size, sockaddr)) = socket.recv_from(&mut buffer) else {
+            continue;
+        };
+        let data = str::from_utf8(&buffer[0..packet_size])?;
+        info!(?data, ?sockaddr, "receiver 2:");
+    }
     Ok(())
 }
 
@@ -49,12 +70,12 @@ fn sender() -> Result<()> {
 
     let socket: UdpSocket = socket.into();
 
-    for i in 1..10 {
+    for i in 1..=10 {
         let multicast_addr: Ipv4Addr = format!("239.0.0.{i}").as_str().parse()?;
-        let multicast_destination: SocketAddr = (multicast_addr, PORT).into();
+        let multicast_sockaddr: SocketAddr = (multicast_addr, PORT).into();
         let packet_contents = format!("packet {i}");
-        info!(?packet_contents, %multicast_destination, "sender");
-        socket.send_to(packet_contents.as_bytes(), multicast_destination)?;
+        info!(?packet_contents, %multicast_addr, "sending");
+        socket.send_to(packet_contents.as_bytes(), multicast_sockaddr)?;
         thread::sleep(Duration::from_millis(200));
     }
     Ok(())
